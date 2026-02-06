9759c1aa401ccc8769997359ee054b90a8fbdebc
VARUN KUMAR POTLA <129356069+Var1035@users.noreply.github.com>
2026-02-06 19:55:31 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,11 @@ import json
 import os
 from dotenv import load_dotenv
 
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 load_dotenv()
 
